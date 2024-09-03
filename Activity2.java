ce4d36c62bf2f4bc5eb7ef0da9c151a7563b1a27
Andreigh Espino <andreighespino@yahoo.com>
2024-09-03 15:47:09 +0800

    Activity 1 & 2

---
+++ Activity2.java
@@ -0,0 +1,8 @@
+public class Activity2 {
+    public static void main(String[] args) {
+        int number = 16;
+        String word = "Michael";
+        System.out.println("The number is: " + number);
+        System.out.println("The word is: " + word);
+    }
+}
